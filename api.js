a35bb7e7335a523509f8dad499f472a62db49df5
Francori8 <francoorizonte@gmail.com>
2024-08-04 22:21:09 -0300

    Primer commit, pagina funcionando

---
+++ api.js
@@ -0,0 +1,131 @@
+export default  [
+    {
+        id:1,
+        simbolo: ",",
+        temas:["coma"]
+
+    },
+    {
+        id:2,
+        simbolo: ".",
+        temas:["punto"]
+
+    },
+    {
+        id:4,
+        simbolo: ":",
+        temas:["dos puntos"]
+
+    },
+    {
+        id: 5,
+        simbolo: ";",
+        temas:["punto y coma"]
+    },
+    
+    {
+        id:6,
+        simbolo: "`",
+        temas:["comilla invertida","string literals","comilla francesa","backtick"]
+
+    },
+    {
+        id:7,
+        simbolo: "'",
+        temas:["comilla simple"]
+
+    },
+    {
+        id:8,
+        simbolo: '"',
+        temas:["comilla doble"]
+
+    },
+    {
+        id:9,
+        simbolo:"{",
+        temas:["llave izquierda", "bloque"]
+    },
+    {
+        id:10,
+        simbolo:"}",
+        temas:["llave derecha", "bloque"]
+    },
+    {
+        id:11,
+        simbolo:"[",
+        temas:["corchete izquierdo", "bloque"]
+    },
+    {
+        id:12,
+        simbolo:"]",
+        temas:["corchete derecho", "bloque"]
+    },
+    {
+        id:13,
+        simbolo:"(",
+        temas:["parentesis izquierdo", "bloque"]
+    },
+    {
+        id:14,
+        simbolo:")",
+        temas:["parentesis derecho", "bloque"]
+    },
+    {
+        id:15,
+        simbolo:"=",
+        temas:["igualdad","comparacion","asignacion"]
+
+    },
+    {
+        id:16,
+        simbolo:"<",
+        temas:["menor","comparacion"]
+
+    },
+    {
+        id:17,
+        simbolo:">",
+        temas:["mayor","comparacion"]
+
+    },
+    
+    {
+        id:18,
+        simbolo:"+",
+        temas:["suma","operacion aritmetica"]
+
+    },
+    {
+        id:19,
+        simbolo:"-",
+        temas:["menos","negacion","operacion aritmetica","guion medio"]
+
+    },
+    {
+        id:20,
+        simbolo:"*",
+        temas:["multiplicar","por","operacion aritmetica","asterisco"]
+
+    },
+    {
+        id:21,
+        simbolo:"/",
+        temas:["division","dividir","operacion aritmetica","barra inclinada"]
+
+    },
+    {
+        id:22,
+        simbolo:"^",
+        temas:["potencia","sombrerito","operacion aritmetica","retornar"]
+
+    },
+    {
+        id:23,
+        simbolo:"√",
+        temas:["raiz","operacion aritmetica"]
+
+    }
+    
+    
+]
---
+++ script.js
@@ -0,0 +1,100 @@
+import simbolo from "./api.js"
+
+const $ = (args) => document.querySelector(args)
+$("#buscar").addEventListener("input",mostrarContenidoFiltrado)
+
+function cargar(){
+    mostrarSimbolos(simbolo)
+}
+
+function mostrarSimbolos(simbolos){
+   const fav = JSON.parse(localStorage.getItem("favoritos")) || []
+   const simbolosOrdenadosSegunFavoritos = ordenarSiEstanFavoritos(simbolos, fav) 
+   const simbolosHTML = simbolos.map(val => {
+        return `<div class="tarjeta" title="${val.temas[0]}" >
+                    <div class="subtitulo" >
+                        <h2>${val.simbolo}</h2>
+                    </div>
+                    <div class="botones">
+                        <button class="btn copy" data-valor="${val.simbolo}" title="Copiar" >🔗</button>
+                        <button class="btn fav ${fav.includes(val.id +"") ? "corazon"  : "" }" data-id="${val.id}" title="Me Gusta">❤</button>
+                    </div>
+                    <div class="notificar"></div>
+                </div>`
+    })
+
+    const contenedor = $("#contenedor-simbolos")
+
+    contenedor.innerHTML = simbolosHTML.join("")
+
+    contenedor.querySelectorAll(".copy").forEach(btn => {
+        btn.addEventListener("click", copiarSimbolo)
+    })
+
+    contenedor.querySelectorAll(".fav").forEach(btn => {
+        btn.addEventListener("click", cambiarfavoritos)
+    })
+
+    
+}
+
+function cambiarfavoritos(e){
+    const idSimb = e.target.dataset.id
+    const fav = JSON.parse(localStorage.getItem("favoritos")) || []
+    
+    if(fav.includes(idSimb)){
+
+        e.target.classList.remove("corazon")
+        fav.splice(fav.indexOf(idSimb),1)
+        localStorage.removeItem("favoritos")
+        localStorage.setItem("favoritos",JSON.stringify(fav))
+
+    }else{
+        e.target.classList.add("corazon")
+        fav.push(idSimb)
+        localStorage.removeItem("favoritos")
+        localStorage.setItem("favoritos",JSON.stringify(fav))
+    }
+}
+
+async function copiarSimbolo(e){
+    const simbolo = e.target.dataset.valor
+    try{
+        await navigator.clipboard.writeText(simbolo)
+        escribirEnPop(e,"Copiado")
+    }catch(error){
+        escribirEnPop(e,"No se pudo copiar")
+    }
+}
+
+
+function ordenarSiEstanFavoritos(simbolos, fav){
+    return simbolos.sort((a,b) => {
+        if(fav.includes( a.id + "")){
+            return -1
+        }
+        return 1
+    })
+}
+
+function escribirEnPop(elemento , mensaje){
+    
+    const contexto = elemento.target.parentNode.parentNode
+    const eleemntoNotificador  = contexto.querySelector(".notificar")
+    contexto.classList.add("visualizarPop")
+    eleemntoNotificador.classList.add("visualizar")
+    eleemntoNotificador.innerText = mensaje
+    setTimeout(() => {
+        eleemntoNotificador.classList.remove("visualizar")
+        contexto.classList.remove("visualizarPop")
+    }, 500)
+    
+}
+
+function mostrarContenidoFiltrado(e){
+    const filtro = e.target.value.toLowerCase()
+    const simbolosMostar = simbolo.filter(val => val.temas.some(string => string.includes(filtro)))
+    mostrarSimbolos(simbolosMostar)
+}
+
+window.addEventListener("load",cargar)
